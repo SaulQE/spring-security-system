f18e3753f5c48218adf4eb0b28b4cd1958122b43
SaulQE <saul.esquivel.028@gmail.com>
2024-04-25 01:09:07 -0500

    add RolController

---
+++ src/main/java/com/quispesucso/controller/RolController.java
@@ -0,0 +1,25 @@
+package com.quispesucso.controller;
+
+import com.quispesucso.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class RolController
+{
+    @Autowired
+    private RoleService roleService;
+
+    public RolController(){}
+
+    @GetMapping("/dba/roles")
+    public String listar_GET(Map map)
+    {
+        map.put("rolesDb", roleService.findAll());
+        return "Role/listar";
+    }
+
+}
